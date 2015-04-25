1db335156c71e53e97d72724db2dde003edf6486
Baptiste <baptiste.truchot@gmail.com>
2015-04-25 13:28:53 +0200

    Initial Commit

---
+++ FlashTape/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FlashTape
+//
+//  Created by Baptiste Truchot on 4/25/15.
+//  Copyright (c) 2015 Mindie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FlashTape/MasterViewController.h
@@ -0,0 +1,15 @@
+//
+//  MasterViewController.h
+//  FlashTape
+//
+//  Created by Baptiste Truchot on 4/25/15.
+//  Copyright (c) 2015 Mindie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MasterViewController : UITableViewController
+
+
+@end
+
