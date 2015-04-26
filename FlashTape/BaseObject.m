3a2c519bb0724d0971091d4dc0c26f416372e32d
Baptiste <baptiste.truchot@gmail.com>
2015-04-26 20:06:06 +0200

    missing file in previous commit

---
+++ FlashTape/BaseObject.m
@@ -0,0 +1,13 @@
+//
+//  BaseObject.m
+//  FlashTape
+//
+//  Created by Baptiste Truchot on 4/26/15.
+//  Copyright (c) 2015 Mindie. All rights reserved.
+//
+
+#import "BaseObject.h"
+
+@implementation BaseObject
+
+@end
